cef09d8cc775cd29d5a89d2a2ea7434dc299fc11
Your Name <abdelsatarahmed77777@gmail.com>
2022-08-30 15:33:48 +0200

    add Pie chart

--- src/components/challenge3/challege3.jsx
+++ src/components/challenge3/challege3.jsx
@@ -1,17 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useEffect, useState } from "react";
-import { Bar } from "react-chartjs-2";
+import { Bar, Pie } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 const Challenge3 = () => {
   const [users, setUsers] = useState([]);
+
+  var [mens, setMens] = useState(0);
+  var [womans, setWomans] = useState(0);
+
 
   useEffect(() => {
     fetch(`https://randomuser.me/api/?results=100`)
       .then((res) => res.json())
       .then((res) => {
         setUsers(res.results);
+        users.map(user => {
+          if(user.gender == 'male')
+            setMens(mens++);
+          else
+            setWomans(womans++);  
+        })
       });
   }, []);
+
+
 
   return (
     <>
@@ -33,24 +45,33 @@ const Challenge3 = () => {
         }}
         height={64}
       />
-       <Bar
+      <div style={{width:'300px'}}>
+      <Pie
+       width={20}
         data={{
-          labels: users.map((user) => user.name.first),
-
+          labels: ['Female', 'Male'],
           datasets: [
             {
-              label: "REGISTERED AGE",
-              data: users.map((user) => user.registered.age),
-              backgroundColor:[
-                '#435378',
-                '#963839',
-                '#226335'
-              ]
+              label: '# of Votes',
+              data: [womans, mens],
+              backgroundColor: [
+               'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 159, 64, 0.5)',
+              ],
+              borderWidth: 1,
+              offset:7
             },
           ],
         }}
-        height={64}
       />
+      </div>
+       
     </>
   );
 };
